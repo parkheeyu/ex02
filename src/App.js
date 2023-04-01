67b671af1f75d96e057f22c8c493ee6bacf1a5cc
parkheeyu <qkrgmldbs1@naver.com>
2023-04-01 10:18:45 +0900

    ver01Router

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RouterPage from './Components/RouterPage';
+
+function App() {
+    return (
+        <div className="App">
+            <RouterPage/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/PostsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostsPage = () => {
+  return (
+    <div>
+        <h1>게시글</h1>
+        
+    </div>
+  )
+}
+
+export default PostsPage
